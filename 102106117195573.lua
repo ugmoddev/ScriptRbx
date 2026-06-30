21e326a2dea7f4e3242e9195f3ff8bfbbd74b5b8
Dev-AuraHub <anitagonzalezdavis750519@gmail.com>
2026-06-30 12:35:18 +0700

    Update 102106117195573.lua

--- 102106117195573.lua
+++ 102106117195573.lua
@@ -1,4 +1,4 @@
 --web trieu
 loadstring(game:HttpGet('https://pastefy.app/JVk4bYLW/raw'))()
 --web obf
-loadstring(game:HttpGet('https://pastefy.app/1RC7mcsx/raw'))()
+loadstring(game:HttpGet('https://pastefy.app/rt6EonmR/raw'))()
