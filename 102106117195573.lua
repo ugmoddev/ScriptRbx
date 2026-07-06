6ee6616376d40c6fe01822018cf491871afa205b
Dev-AuraHub <244536429+ugmoddev@users.noreply.github.com>
2026-07-06 10:55:00 +0700

    Update 102106117195573.lua

--- 102106117195573.lua
+++ 102106117195573.lua
@@ -1,6 +1,18 @@
 -- noitify tu lam
-loadstring(game:HttpGet("https://pastefy.app/TOV3h26F/raw"))()
+task.spawn(function()
+    pcall(function()
+        loadstring(game:HttpGet("https://pastefy.app/TOV3h26F/raw"))()
+    end)
+end)
 -- noitify trieu
-loadstring(game:HttpGet("https://pastefy.app/UdwqXxuu/raw"))()
+task.spawn(function()
+    pcall(function()
+        loadstring(game:HttpGet("https://pastefy.app/UdwqXxuu/raw"))()
+    end)
+end)
 -- noitify tuất nguyến 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Dev-AnhTuansitink/NatAov-Hub/refs/heads/main/Trom_Joibid_Hub_May.lua"))()
+task.spawn(function()
+    pcall(function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Dev-AnhTuansitink/NatAov-Hub/refs/heads/main/Trom_Joibid_Hub_May.lua"))()
+    end)
+end)
