93fd97e8905c26616296ecf92dc856d469154088
Dev-AuraHub <244536429+ugmoddev@users.noreply.github.com>
2026-07-07 11:08:19 +0700

    Remove outdated loadstring call
    Removed a call to loadstring for a specific URL.

--- 102106117195573.lua
+++ 102106117195573.lua
@@ -2,8 +2,6 @@
 pcall(loadstring(game:HttpGet"https://pastefy.app/TOV3h26F/raw"))
 -- noitify trieu
 pcall(loadstring(game:HttpGet"https://pastefy.app/UdwqXxuu/raw"))
--- noitify tuất nguyến 
-pcall(loadstring(game:HttpGet"https://raw.githubusercontent.com/Dev-AnhTuansitink/NatAov-Hub/refs/heads/main/Trom_Joibid_Hub_May.lua"))
 -- hncu
 pcall(loadstring(game:HttpGet"https://raw.githubusercontent.com/Dev-Hinishi/Hinishi-Hub/refs/heads/main/API/CheckBoss"))
 --web tự làm api triệu
