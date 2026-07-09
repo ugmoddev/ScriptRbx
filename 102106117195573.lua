099591ec3b28da3f3def20f23bcb8ef1d5d61a48
Dev-AuraHub <244536429+ugmoddev@users.noreply.github.com>
2026-07-09 12:33:49 +0700

    Update 102106117195573.lua

--- 102106117195573.lua
+++ 102106117195573.lua
@@ -6,3 +6,5 @@ pcall(loadstring(game:HttpGet"https://pastefy.app/j9c355KS/raw"))
 pcall(loadstring(game:HttpGet"https://raw.githubusercontent.com/Dev-Hinishi/Hinishi-Hub/refs/heads/main/API/CheckBoss"))
 --web tự làm api triệu
 pcall(loadstring(game:HttpGet"https://obfuscatorhub.onrender.com/api/nvbs0eoo"))
+-- tày hút
+pcall(loadstring(game:HttpGet"https://raw.githubusercontent.com/VTDROBLOX/Animehub/refs/heads/main/API.lua"))
