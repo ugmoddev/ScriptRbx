263ea18ab619e691405b92df0cf9f8a28c97ae5b
Dev-AuraHub <244536429+ugmoddev@users.noreply.github.com>
2026-07-16 11:11:31 +0700

    Update 102106117195573.lua

--- 102106117195573.lua
+++ 102106117195573.lua
@@ -1,7 +1,7 @@
 -- noitify tu lam
-pcall(loadstring(game:HttpGet"https://pastefy.app/KYSWVDIn/raw"))
+pcall(loadstring(game:HttpGet"https://pastefy.app/hXdjC9PS/raw"))
 -- noitify trieu
-pcall(loadstring(game:HttpGet"https://pastefy.app/mkV44TVa/raw"))
+pcall(loadstring(game:HttpGet"https://pastefy.app/riG3LaZS/raw"))
 -- hncu
 pcall(loadstring(game:HttpGet"https://raw.githubusercontent.com/Dev-Hinishi/Hinishi-Hub/refs/heads/main/API/CheckBoss"))
 --web tự làm api triệu
@@ -9,4 +9,4 @@ pcall(loadstring(game:HttpGet"https://obfuscatorhub.onrender.com/api/nvbs0eoo"))
 -- tày hút
 pcall(loadstring(game:HttpGet"https://raw.githubusercontent.com/VTDROBLOX/Animehub/refs/heads/main/API.lua"))
 -- loadstring counter
-pcall(loadstring(game:HttpGet"https://pastefy.app/ty4g0GjC/raw"))
+pcall(loadstring(game:HttpGet"https://pastefy.app/eEL7VFy4/raw"))
