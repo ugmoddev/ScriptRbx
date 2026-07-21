933fd0ba4bcf9dcea526f844fb970f6c9370a887
Dev-NejiHub <244536429+ugmoddev@users.noreply.github.com>
2026-07-21 12:53:58 +0700

    Update 102106117195573.lua

--- 102106117195573.lua
+++ 102106117195573.lua
@@ -1,11 +1,7 @@
--- noitify tu lam
-pcall(loadstring(game:HttpGet"https://pastefy.app/hXdjC9PS/raw"))
--- noitify trieu
-pcall(loadstring(game:HttpGet"https://pastefy.app/riG3LaZS/raw"))
 -- hncu
 pcall(loadstring(game:HttpGet"https://raw.githubusercontent.com/Dev-Hinishi/Hinishi-Hub/refs/heads/main/API/CheckBoss"))
---web tự làm api triệu
-pcall(loadstring(game:HttpGet"https://obfuscatorhub.onrender.com/api/nvbs0eoo"))
+--web tự làm noitify tu lam
+pcall(loadstring(game:HttpGet"https://pastefy.app/NVabig9z/raw"))
 -- tày hút
 pcall(loadstring(game:HttpGet"https://raw.githubusercontent.com/VTDROBLOX/Animehub/refs/heads/main/API.lua"))
 -- loadstring counter
