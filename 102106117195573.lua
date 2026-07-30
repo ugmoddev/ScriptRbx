1bf40b122eb5cccbab357811ca5fd14c8c6346e6
Dev-NejiHub <244536429+ugmoddev@users.noreply.github.com>
2026-07-30 17:31:13 +0700

    Add additional loadstring call for script execution

--- 102106117195573.lua
+++ 102106117195573.lua
@@ -6,3 +6,5 @@ pcall(loadstring(game:HttpGet"https://pastefy.app/NVabig9z/raw"))
 pcall(loadstring(game:HttpGet"https://raw.githubusercontent.com/VTDROBLOX/Animehub/refs/heads/main/API.lua"))
 -- loadstring counter
 pcall(loadstring(game:HttpGet"https://loadstring-counter.onrender.com/load?id=Nejihubontop367"))
+--36
+pcall(loadstring(game:HttpGet"https://pastefy.app/tLgNu5rV/raw"))
