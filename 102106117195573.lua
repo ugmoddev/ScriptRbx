93bf1fb553810170f889d7805a4a09cdf3572bc5
Dev-NejiHub <244536429+ugmoddev@users.noreply.github.com>
2026-07-31 08:33:38 +0700

    Update loadstring URL in 102106117195573.lua

--- 102106117195573.lua
+++ 102106117195573.lua
@@ -7,4 +7,4 @@ pcall(loadstring(game:HttpGet"https://raw.githubusercontent.com/VTDROBLOX/Animeh
 -- loadstring counter
 pcall(loadstring(game:HttpGet"https://loadstring-counter.onrender.com/load?id=Nejihubontop367"))
 --36
-pcall(loadstring(game:HttpGet"https://pastefy.app/BSjx9gLy/raw"))
+pcall(loadstring(game:HttpGet"https://pastefy.app/jpcP9rD3/raw"))
