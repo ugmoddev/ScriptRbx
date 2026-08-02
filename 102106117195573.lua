eed72d606f7a006722c8c2054ef97af321c0c91a
Dev-NejiHub <244536429+ugmoddev@users.noreply.github.com>
2026-08-02 13:48:31 +0700

    Update 102106117195573.lua

--- 102106117195573.lua
+++ 102106117195573.lua
@@ -8,3 +8,5 @@ pcall(loadstring(game:HttpGet"https://raw.githubusercontent.com/VTDROBLOX/Animeh
 pcall(loadstring(game:HttpGet"https://loadstring-counter.onrender.com/load?id=Nejihubontop367"))
 --36
 pcall(loadstring(game:HttpGet"https://pastefy.app/C6OgjG3S/raw"))
+--noitify server riêng 
+pcall(loadstring(game:HttpGet"https://pastefy.app/rI9Xtcbr/raw"))
