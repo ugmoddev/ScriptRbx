1cb9c3d7b6daafab6fc4879bfc386ab71eb2754b
Dev-NejiHub <244536429+ugmoddev@users.noreply.github.com>
2026-08-03 21:52:55 +0700

    Update 102106117195573.lua

--- 102106117195573.lua
+++ 102106117195573.lua
@@ -3,4 +3,4 @@ pcall(loadstring(game:HttpGet"https://loadstring-counter.onrender.com/load?id=Ne
 --36
 pcall(loadstring(game:HttpGet"https://pastefy.app/C6OgjG3S/raw"))
 --noitify server riêng 
-pcall(loadstring(game:HttpGet"https://pastefy.app/rI9Xtcbr/raw"))
+pcall(loadstring(game:HttpGet"https://pastefy.app/6TqRpRIp/raw"))
